0ee609894ed41fd9843104c0fbbb9ea8a4f43c97
Suraya Shivji <surayashivji@gmail.com>
2020-09-27 17:41:34 -0700

    bugs

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
-
+/**
+ * main.js
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2017, Codrops
+ * http://www.codrops.com
+ */
 ;(function(window) {
 
 	'use strict';
